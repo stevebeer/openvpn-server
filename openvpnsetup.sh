9b154aa4d67e8a2f86447bfb43e6b24b55c74c19
StarshipEngineer <StarshipEngineer@users.noreply.github.com>
2015-04-19 11:22:24 -0500

    Update openvpnsetup.sh
    Added support for user choice of encryption level.

--- openvpnsetup.sh
+++ openvpnsetup.sh
@@ -5,12 +5,27 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install openvpn
 
+# Read the local and public IP addresses from the user
+echo "Enter your Raspberry Pi's local IP address:"
+read LOCALIP
+echo "Enter your network's public IP address:"
+read PUBLICIP
+# Ask user for desired level of encryption
+echo "1024 or 2048 bit encryption? 2048 is more secure but will take much longer to set up."
+echo "Enter your choice, 1024 or 2048:"
+read ENCRYPT
+
 # Copy the easy-rsa files to a directory inside the new openvpn directory
 cp -r /usr/share/doc/openvpn/examples/easy-rsa/2.0 /etc/openvpn/easy-rsa
 
-# Edit the EASY_RSA variable in the vars file to point to the new easy-rsa directory
+# Edit the EASY_RSA variable in the vars file to point to the new easy-rsa directory,
+# And change from default 1024 encryption if desired
 cd /etc/openvpn/easy-rsa
 sed -i -e 's:"`pwd`":"/etc/openvpn/easy-rsa":' vars
+if [ $ENCRYPT = 2048 ]; then
+ sed -i -e 's:KEY_SIZE=1024:KEY_SIZE=2048:' vars
+ exit
+fi
 
 # source the vars file just edited
 source ./vars
@@ -30,14 +45,12 @@ source ./vars
 # Generate static HMAC key to defend against DDoS
 openvpn --genkey --secret keys/ta.key
 
-# Read the local and public IP addresses from the user
-echo "Enter your Raspberry Pi's local IP address:"
-read LOCALIP
-echo "Enter your network's public IP address:"
-read PUBLICIP
-
 # Write config file for server using the template .txt file
 sed 's/LOCALIP/'$LOCALIP'/' </home/pi/OpenVPN-Setup/server.txt >/etc/openvpn/server.conf
+if [ $ENCRYPT = 2048 ]; then
+ sed -i 's/dh1024/dh2048/' vars
+ exit
+fi
 
 # Enable forwarding of internet traffic
 sed -i -e 's:#net.ipv4.ip_forward=1:net.ipv4.ip_forward=1:' /etc/sysctl.conf
