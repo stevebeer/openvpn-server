808336c1c658156cc6e96977f594f1cae568051d
Dan Shuman <StarshipEngineer@users.noreply.github.com>
2015-04-20 16:16:14 -0500

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Remove openvpn
+apt-get -y remove openvpn
+
+# Remove directories
+rm -r /etc/openvpn /home/pi/ovpns
+
+# Remove firewall script and reference to it in interfaces
+sed -i '/firewall-openvpn-rules.sh/d' /etc/network/interfaces
+rm /etc/firewall-openvpn-rules.sh
+
+# Disable IPv4 forwarding
+sed -i 's:net.ipv4.ip_forward=1:#net.ipv4.ip_forward=1:' /etc/sysctl.conf
+sysctl -p
