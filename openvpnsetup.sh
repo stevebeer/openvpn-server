cba48126b69c77a622637856747711a04b45ccb6
Dan Shuman <StarshipEngineer@users.noreply.github.com>
2015-04-25 20:42:37 -0500

    Update openvpnsetup.sh

--- openvpnsetup.sh
+++ openvpnsetup.sh
@@ -3,7 +3,7 @@
 exitstatus=0
 while [ $exitstatus = 0 ]
 do
-	CHOICE=$(whiptail --title "OpenVPN-Setup" --menu "Setup options:" 16 78 5 \
+	CHOICE=$(whiptail --title "OpenVPN-Setup" --menu "Setup options:" 16 78 3 \
 	"01" "Setup OpenVPN on your system" \
 	"02" "Generate a client profile with MakeOVPN" \
 	"03" "Remove OpenVPN and revert your system to a pre-installation state" 3>&2 2>&1 1>&3)
@@ -21,6 +21,9 @@ do
 		;;
 		03)
 			sudo ./remove.sh
+			if (whiptail --title "Reboot" --yesno --defaultno "Would you like to reboot now?" 8 78) then
+				whiptail --title "Reboot" --msgbox "The system will reboot." 8 78
+			fi
 		;;
 		*) #whiptail --title "OpenVPN-Setup" --msgbox "You cancelled or are done." 8 78
 			exitstatus=1
